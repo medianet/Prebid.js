166de75369fa09c5296861b37743dbba5737082f
PsychodelEKS <psychodeleks@gmail.com>
2017-05-15 18:12:54 +0300

    #3112: Подключить новую HB-систему demand_source=40 [ https://clickio.tpondemand.com/entity/3112 ]

--- gulpfile.js
+++ gulpfile.js
@@ -25,7 +25,7 @@ var optimizejs = require('gulp-optimize-js');
 
 var CI_MODE = process.env.NODE_ENV === 'ci';
 var prebid = require('./package.json');
-var dateString = 'Updated : ' + (new Date()).toISOString().substring(0, 10);
+var dateString = 'Updated : ' + (new Date()).toISOString().split('T').join(' ').substring(0, 19);
 var packageNameVersion = prebid.name + '_' + prebid.version;
 var banner = '/* <%= prebid.name %> v<%= prebid.version %>\n' + dateString + ' */\n';
 var analyticsDirectory = '../analytics';
